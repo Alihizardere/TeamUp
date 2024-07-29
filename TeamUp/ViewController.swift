7d5b3583f281680b1358ca16d882d90257bdbd91
Ali Hızardere <aliduyguhzr@gmail.com>
2024-07-29 16:59:53 +0300

    initial commit

---
+++ TeamUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamUp
+//
+//  Created by alihizardere on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
