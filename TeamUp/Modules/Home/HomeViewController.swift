dcdee245304e3efbe199c1fcf403b9d03c8bd302
dtemizyurek <dogukantemizyurek@gmail.com>
2024-07-29 18:41:36 +0300

    Home and landing page has created

---
+++ TeamUp/Modules/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  TeamUp
+//
+//  Created by Doğukan Temizyürek on 29.07.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is Home page")
+    }
+
+}
---
+++ TeamUp/Modules/Landing/LandingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LandingViewController.swift
+//  TeamUp
+//
+//  Created by Doğukan Temizyürek on 29.07.2024.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is landing page")
+    }
+
+}
