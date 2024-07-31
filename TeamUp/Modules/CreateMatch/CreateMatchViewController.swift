723b3187ee20171a95a24d60c9b7d406229d82e4
dtemizyurek <dogukantemizyurek@gmail.com>
2024-07-31 19:55:21 +0300

    Weather widget icon and functions added retrieve actual location from user

--- TeamUp/Modules/CreateMatch/CreateMatchViewController.swift
+++ TeamUp/Modules/CreateMatch/CreateMatchViewController.swift
@@ -6,18 +6,25 @@
 //
 
 import UIKit
+import CoreLocation
 
 final class CreateMatchViewController: UIViewController {
     //MARK: - Properties
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var lblLocation: UILabel!
+    @IBOutlet weak var lblTemprature: UILabel!
+    @IBOutlet weak var lblDesc: UILabel!
+    @IBOutlet weak var tempImage: UIImageView!
+    
     var players = [Player]()
+    private let locationManager = CLLocationManager()
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UINib(nibName: PlayerCell.identifier, bundle: nil), forCellReuseIdentifier: PlayerCell.identifier)
+        setupTableView()
+        setupLocationManager()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,6 +33,18 @@ final class CreateMatchViewController: UIViewController {
     }
     
     //MARK: - Private Functions
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: PlayerCell.identifier, bundle: nil), forCellReuseIdentifier: PlayerCell.identifier)
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
     private func fetchPlayers() {
         FirebaseService.shared.fetchPlayerKeys { keys in
             self.players.removeAll()
@@ -45,6 +64,45 @@ final class CreateMatchViewController: UIViewController {
         }
     }
     
+    private func fetchWeather(for location: CLLocation) {
+        WeatherLogic.shared.fetchWeather(
+            latitude: location.coordinate.latitude,
+            longitude: location.coordinate.longitude
+        ) { [weak self] result in
+            switch result {
+            case .success(let weatherResponse):
+                DispatchQueue.main.async {
+                    if let temp = weatherResponse.main?.temp {
+                        self?.lblTemprature.text = "\(temp)°C"
+                    }
+                    if let weather = weatherResponse.weather?.first {
+                        self?.lblDesc.text = weather.description?.capitalized
+                        self?.updateWeatherImage(for: weather)
+                        print("Weather description from API: \(weather.description ?? "")")
+                    }
+                }
+            case .failure(let error):
+                print("Failed to fetch weather data: \(error)")
+            }
+        }
+    }
+
+    private func updateWeatherImage(for weather: Weather) {
+        switch weather.main?.lowercased() {
+        case "rain":
+            tempImage.image = UIImage(systemName: "cloud.rain")
+        case "clear":
+            tempImage.image = UIImage(systemName: "sun.min.fill")
+            tempImage.tintColor = .systemYellow
+        case "clouds":
+            tempImage.image = UIImage(systemName: "cloud.fill")
+            tempImage.tintColor = .systemGray3
+        default:
+            tempImage.image = UIImage(systemName: "questionmark.circle")
+        }
+    }
+
+    
     //MARK: - Button Actions
     @IBAction func createMatchButtonTapped(_ sender: Any) {
         let matchDetailVC = MatchDetailViewController(nibName: "MatchDetailViewController", bundle: nil)
@@ -52,7 +110,7 @@ final class CreateMatchViewController: UIViewController {
     }
 }
 
-   //MARK: - Extension TableViewDelegate && TableViewDataSource
+//MARK: - Extension TableViewDelegate && TableViewDataSource
 extension CreateMatchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         players.count
@@ -63,5 +121,27 @@ extension CreateMatchViewController: UITableViewDelegate, UITableViewDataSource 
         let player = players[indexPath.row]
         cell.configure(with: player)
         return cell
+    }
+}
+
+//MARK: - ExtensionCLLocationManagerDelegate
+extension CreateMatchViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks,error) in
+            guard let self = self else { return }
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let placemark = placemarks?.first else {return }
+            self.lblLocation.text = [placemark.locality, placemark.administrativeArea].compactMap{ $0 }.joined(separator: ", ")
+            self.fetchWeather(for: location)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location: \(error)")
     }
 }
