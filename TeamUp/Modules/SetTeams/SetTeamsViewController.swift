e08334069b2e16a708b45cc1d106efd6e46da018
dtemizyurek <dogukantemizyurek@gmail.com>
2024-08-06 00:01:56 +0300

    SetTeamsViewController has created

---
+++ TeamUp/Modules/SetTeams/SetTeamsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SetTeamsViewController.swift
+//  TeamUp
+//
+//  Created by Doğukan Temizyürek on 5.08.2024.
+//
+
+import UIKit
+
+class SetTeamsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
